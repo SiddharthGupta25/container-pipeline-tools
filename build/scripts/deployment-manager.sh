9888744f19da8108e57f9db7b2a61e3da06c2204
SiddharthGupta25 <gsiddharth47@gmail.com>
2023-07-19 20:16:21 +0530

    Added nodes

---
+++ build/scripts/deployment-manager.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+
+if [ $# -ne 1 ]; then
+    echo "[-] No path specified"
+    echo "Usage: $0 <[chrome|firefox|edge|grid]>"
+    exit 1
+fi
+
+
+SELENIUM_NODE_NAME=$1
+APPLICATION_DEPLOYMENT_FILENAME="../../application-deployment.yaml"
+
+
+sed -i "s|path:.*|path: $SELENIUM_NODE_NAME|g" $APPLICATION_DEPLOYMENT_FILENAME
+echo "[+] Path set to $SELENIUM_NODE_NAME"
+
+echo "[*] Applying the deployment to cluster"
+#kubectl apply -f $deployment_yaml
+
+echo "[*] Waiting for deployment to complete"
+#kubectl rollout status deployment/my-app
+
+echo "[+] Deployment completed"
+
+echo "--------------------------"
+echo "[+] DEPLOYED SUCCESSFULLY"
+echo "---------------------------"
